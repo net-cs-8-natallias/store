5bc2601e13d9747dda63bb34d68e5e82ae5ca833
Nataliia Stasiukova <nstasiukova@gmail.com>
2024-01-22 05:46:58 +0200

    adding identity server

---
+++ Store/IdentityServer/IdentityServer/Config.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("catalog")
+                {
+                    Scopes = new List<Scope>
+                    {
+                        new Scope("catalogitem")
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[]
+            {
+                new Client
+                {
+                    ClientId = "catalog",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                    
+                },
+                new Client
+                {
+                    ClientId = "catalogswaggerui",
+                    ClientName = "Catalog Swagger UI",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RedirectUris = { $"http://localhost:5288/swagger/oauth2-redirect.html" },
+                    PostLogoutRedirectUris = { $"http://localhost:5288/swagger/" },
+
+                    AllowedScopes =
+                    {
+                        "catalogitem"
+                    }
+                }
+            };
+        }
+    }
+}
