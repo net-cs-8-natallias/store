dbfba1324e058b19c429530e5afba21ceec0e60b
Nataliia Stasiukova <nstasiukova@gmail.com>
2024-02-12 06:29:51 +0200

    adding tests for services in catalog-service

---
+++ Store/Catalog.Host.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Catalog.Host.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
